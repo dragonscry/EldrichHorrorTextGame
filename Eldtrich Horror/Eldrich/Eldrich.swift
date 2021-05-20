87bb48afcce76c71e4d97420fadae665b5045dad
Denis <denismerkotun@gmail.com>
2021-05-20 22:05:59 +0300

    templates for Eldrich and Monsters

---
+++ Eldtrich Horror/Eldrich/Eldrich.swift
@@ -0,0 +1,13 @@
+//
+//  Eldrich.swift
+//  Eldtrich Horror
+//
+//  Created by  Denis on 20.05.2021.
+//
+
+import Foundation
+
+struct Eldrich {
+    
+}
+
---
+++ Eldtrich Horror/Monsters/Monster.swift
@@ -0,0 +1,17 @@
+//
+//  Monster.swift
+//  Eldtrich Horror
+//
+//  Created by  Denis on 20.05.2021.
+//
+
+import Foundation
+
+struct Monster {
+    
+}
+
+struct EpicMonster
+{
+    
+}
