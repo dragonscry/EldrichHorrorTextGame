7bd28ff32bab27271edd3e8e246bde36dbd78f0d
dragonscry <denismerkotun@gmail.com>
2021-07-13 23:05:13 +0300

    template for market

---
+++ Eldtrich Horror/Market.swift
@@ -0,0 +1,24 @@
+//
+//  Market.swift
+//  Eldtrich Horror
+//
+//  Created by Denys on 13.07.2021.
+//
+
+import Foundation
+
+class Trader{
+    var items : [String] // Change to Active
+    var market = [String]() // Change to Active
+    
+    func addingItemIntoMarket(){
+        while market.count<5{
+            let item = items.remove(at: Int.random(in: 0..<items.count))
+            market.append(item)
+        }
+    }
+    init(items : [String]){
+        self.items = items
+    }
+}
+
